38d6a6084221d4670f373d6789b4a5f565d704eb
Heitor <heitorao32@gmail.com>
2023-11-27 16:04:17 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,5 @@
+export let API_KEY = 'https://api.themoviedb.org/3/movie/12?api_key=0157f2451498ca77f6c5e7dca8be5fce';
+export const BASE_URL = '';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w200/';
+export const language = 'language=pt-BR';
+
